ad3718fe3e0f8122fb550677294202b1bb8cfcd0
rolle <zxcnepo@gmail.com>
2013-07-03 05:04:33 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <SFML/System.hpp>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+int main(int argc, char **argv)
+{
+   sf::Clock clock;
+   std::cout << clock.getElapsedTime().asSeconds()<<std::endl;
     return 0;
 }
